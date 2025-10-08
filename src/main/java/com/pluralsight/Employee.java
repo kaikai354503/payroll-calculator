7503fef1cdefd62bd0c247a0809de6dad77959e3
kai elijah gronholz <kaielijah2113@gmail.com>
2025-10-08 10:07:14 -0500

    Added a file writer to take info from employee.csv and write it into a new file.

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,8 +1,55 @@
 package com.pluralsight;
 
 public class Employee {
+    private int id;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+    private double grossPay;
+
     public Employee(int id, String name, double hoursWorked, double payRate) {
 
+        this.id =id;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+        this.grossPay = payRate * hoursWorked;
 
+
+    }
+
+    public int getId(){
+        return this.id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public Double calculateGrossPay(){
+
+        return this.hoursWorked * this.payRate;
     }
 }
--- src/main/java/com/pluralsight/MainFileio.java
+++ src/main/java/com/pluralsight/MainFileio.java
@@ -1,17 +1,17 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class MainFileio {
     public static void main(String[] args){
 
-        try {
+        try (FileWriter writer = new FileWriter("src/main/resources/payroll.csv")){
 
-            FileReader fileReader = new FileReader("src/main/resorces/employees.csv");
+            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            //writes a new file
+            //FileWriter writer = new FileWriter("src/main/resources/payroll.csv");
 
             //read the header line
             //id|name|hours| payrate
@@ -20,15 +20,20 @@ public class MainFileio {
             //Read the rest of the file
             String input;
             while((input = bufferedReader.readLine()) != null){
-                System.out.println(input);
+
+                //seperates at the "|"
                String[] sections = input.split("\\|");
-                System.out.println(sections);
+                //System.out.println(sections);
 
                 //employee constructor              passes in data from file
-                Employee employee = new Employee(Integer.parseInt(sections[0],sections[1],(Float.parseFloat(sections[2]),(Float.parseFloat(sections[3])));
+                Employee employee = new Employee(Integer.parseInt(sections[0]),sections[1],(Double.parseDouble(sections[2])),(Double.parseDouble(sections[3])));
 
-                System.out.println("Employee named %s made %f", employee.getName, employee.calculateGrossPay());
+                System.out.printf("Employee ID: %d | Name: %s | Gross Pay: $%.2f%n",employee.getId(), employee.getName(),  employee.calculateGrossPay());
+                //writes into new file
+                writer.write("Employee ID: " + employee.getId() + "| " + "Name: " + employee.getName() + "| " + "Gross Pay: " +employee.calculateGrossPay() + "\n");
             }
+
+            bufferedReader.close();
 
         }
 
